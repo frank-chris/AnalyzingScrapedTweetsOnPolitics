1ba43e3d232bd907f50f52620dc681e7591a71d3
frank-chris <chrisfrancischris@gmail.com>
2021-08-30 22:45:41 +0530

    Update heaps law script

--- heaps_law.py
+++ heaps_law.py
@@ -5,7 +5,12 @@ Script to plot heaps law
 
 import pickle
 import plotly.graph_objects as go
+from scipy.optimize import curve_fit
 from tqdm import tqdm
+import numpy as np
+
+def exp_func(x, k, b):
+    return k*(x**b)
 
 open_file = open('tokens_list.pkl', "rb")
 tokens_list = pickle.load(open_file)
@@ -25,6 +30,19 @@ for token in tqdm(tokens_list):
         y.append(len(uniq))
 
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=x, y=y, name='empirical'))
+fig.add_trace(go.Scatter(x=x, y=y, name='empirical curve'))
+
+popt, pcov = curve_fit(exp_func, x, y)
+x_fit = np.linspace(min(x), max(x), 1000)
+y_fit = [exp_func(x, popt[0], popt[1]) for x in x_fit]
+
+fig.add_trace(go.Scatter(x=x_fit, y=y_fit, name='fitted curve'))
+
+anno = "Parameters of fitted curve: K = " + str(round(popt[0], 3)) + ", Beta = " + str(round(popt[1], 3))
+
+fig.update_layout(title_text="|V| vs N (Heaps' Law) - " + anno, 
+                    yaxis_title='|V|', 
+                    xaxis_title='N', 
+                    font_size = 18)
 fig.show()
 
