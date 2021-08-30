4129247355c1ad9ec6253ed34267f8d571fff0d0
frank-chris <chrisfrancischris@gmail.com>
2021-08-30 20:09:51 +0530

    Update heaps law script

--- heaps_law.py
+++ heaps_law.py
@@ -2,15 +2,10 @@
 Chris Francis (chris.francis@iitgn.ac.in)
 Script to plot heaps law
 """
-usage = "Usage:\n\n\
-\
-python3 heaps_law.py PKL_PATH\n\n\
-\
-* PKL_PATH: \n\
-path to tokens list pkl file\n"
 
 import pickle
 import plotly.graph_objects as go
+from tqdm import tqdm
 
 open_file = open('tokens_list.pkl', "rb")
 tokens_list = pickle.load(open_file)
@@ -21,16 +16,15 @@ y = []
 
 uniq = set()
 
-print(len(tokens_list), 'tokens')
-
-for i, token in enumerate(tokens_list):
-    uniq.add(token)
-    x.append(i)
-    y.append(len(uniq))
+i = 0
+for token in tqdm(tokens_list):
+    if not token.startswith('#') and not token.startswith('@'):
+        i+=1
+        uniq.add(token)
+        x.append(i)
+        y.append(len(uniq))
 
 fig = go.Figure()
-
 fig.add_trace(go.Scatter(x=x, y=y, name='empirical'))
-
 fig.show()
 
